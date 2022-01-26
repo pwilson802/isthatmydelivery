a58552953bfd59f7e881f7ea1948a2a7e7c7e08d
Paul Wilson <pwilson802@gmail.com>
2022-01-26 23:10:21 +1100

    added images

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>IS THAT MY DELIVERY?</h1>
+        <div className="wrapper">
+          <Carousel showThumbs={false} showStatus={false} dynamicHeight={true}>
+                <div className="page">
+                    <img src="./assets/gorrilla.png" />
+                </div>
+                <div className="page">
+                    <img src="./assets/gorrilla-red.png" />
+                </div>
+                <div className="page">
+                    <img src="./assets/gorrilla-blue.png" />
+            </div>
+                            <div className="page">
+                    <img src="./assets/gorrilla-bone.png" />
+            </div>
+                            <div className="page">
+                    <img src="./assets/gorrilla-gold.png" />
+            </div>
+            <div className="page">
+                    <img src="./assets/gorrilla-photocopy.png" />
+            </div>
+                        <div className="page">
+                    <img src="./assets/rabbit.png" />
+            </div>
+                                    <div className="page">
+                    <img src="./assets/rabbit-red.png" />
+            </div>
+                                    <div className="page">
+                    <img src="./assets/rabbit-blue.png" />
+            </div>
+                                    <div className="page">
+                    <img src="./assets/rabbit-gold.png" />
+                </div>
+                                    <div className="page">
+                    <img src="./assets/rabbit-red.png" />
+            </div>
+                                    <div className="page">
+                    <img src="./assets/rabbit-photocopy.png" />
+                </div>
+          </Carousel>
+                </div>
       </header>
     </div>
   );
