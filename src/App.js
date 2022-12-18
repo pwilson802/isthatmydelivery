31cb08cde0d31449dac62d18c73e416e25d7c769
Paul Wilson <paul.wilson@unifii.com.au>
2022-12-18 22:19:19 +1100

    updated css queries for larger screens

--- src/App.js
+++ src/App.js
@@ -6,73 +6,73 @@ import { Carousel } from 'react-responsive-carousel';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>IS THAT MY DELIVERY?</h1>
-        <p>Anticipatory pleasure or arrival anxiety? Is That My Delivery explores the exquisite pain and ecstasy of a truly modern phenomena.</p>
-        <p>collection available at <a href="https://opensea.io/collection/is-that-my-delivery" target="_blank">opensea.io</a></p>
-        <div className="wrapper">
-          <Carousel showThumbs={false} showStatus={false} dynamicHeight={true} autoPlay={false} showIndicators={false }>
-                <div className="page">
-                    <img src="./assets/gorrilla.png" />
+        return (
+                <div className="App">
+                        <header className="App-header">
+                                <h1>IS THAT MY DELIVERY?</h1>
+                                <p>Anticipatory pleasure or arrival anxiety? Is That My Delivery explores the exquisite pain and ecstasy of a truly modern phenomena.</p>
+                                <p>collection available at <a href="https://opensea.io/collection/is-that-my-delivery" target="_blank">opensea.io</a></p>
+                                <div className="wrapper">
+                                        <Carousel showThumbs={false} showStatus={false} dynamicHeight={true} autoPlay={false} showIndicators={false}>
+                                                <div className="page">
+                                                        <img src="./assets/gorrilla.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/gorrilla-red.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/gorrilla-blue.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/gorrilla-bone.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/gorrilla-gold.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/gorrilla-photocopy.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/rabbit.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/rabbit-red.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/rabbit-blue.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/rabbit-gold.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/rabbit-red.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/rabbit-photocopy.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/deer.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/deer-blue.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/deer-bone.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/deer-gold.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/deer-photocopy.png" />
+                                                </div>
+                                                <div className="page">
+                                                        <img src="./assets/deer-plum.png" />
+                                                </div>
+                                        </Carousel>
+                                </div>
+                        </header>
                 </div>
-                <div className="page">
-                    <img src="./assets/gorrilla-red.png" />
-                </div>
-                <div className="page">
-                    <img src="./assets/gorrilla-blue.png" />
-            </div>
-                            <div className="page">
-                    <img src="./assets/gorrilla-bone.png" />
-            </div>
-                            <div className="page">
-                    <img src="./assets/gorrilla-gold.png" />
-            </div>
-            <div className="page">
-                    <img src="./assets/gorrilla-photocopy.png" />
-            </div>
-                        <div className="page">
-                    <img src="./assets/rabbit.png" />
-            </div>
-                                    <div className="page">
-                    <img src="./assets/rabbit-red.png" />
-            </div>
-                                    <div className="page">
-                    <img src="./assets/rabbit-blue.png" />
-            </div>
-                                    <div className="page">
-                    <img src="./assets/rabbit-gold.png" />
-                </div>
-                                    <div className="page">
-                    <img src="./assets/rabbit-red.png" />
-            </div>
-                                    <div className="page">
-                    <img src="./assets/rabbit-photocopy.png" />
-            </div>
-            <div className="page">
-                    <img src="./assets/deer.png" />
-                </div>
-              <div className="page">
-                    <img src="./assets/deer-blue.png" />
-            </div>
-            <div className="page">
-                    <img src="./assets/deer-bone.png" />
-            </div>
-            <div className="page">
-                    <img src="./assets/deer-gold.png" />
-            </div>
-            <div className="page">
-                    <img src="./assets/deer-photocopy.png" />
-            </div>
-            <div className="page">
-                    <img src="./assets/deer-plum.png" />
-                </div>
-          </Carousel>
-                </div>
-      </header>
-    </div>
-  );
+        );
 }
 
 export default App;
